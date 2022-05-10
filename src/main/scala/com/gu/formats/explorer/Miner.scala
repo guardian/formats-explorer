e62e43a5315f95c3a5dcc4249fa0ed06096430bd
Roberto Tyley <roberto.tyley@gmail.com>
2022-05-10 11:48:56 +0100

    WIP

--- src/main/scala/com/gu/formats/explorer/Miner.scala
+++ src/main/scala/com/gu/formats/explorer/Miner.scala
@@ -1,18 +1,31 @@
 package com.gu.formats.explorer
 
 import com.gu.contentapi.client.model.SearchQuery
+import com.gu.contentapi.client.model.v1.Content
 import com.gu.contentapi.client.utils.CapiModelEnrichment._
 import com.gu.contentapi.client.{ContentApiClient, GuardianContentClient}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+
+case class Format(
+  theme: String,
+  design: String,
+  display: String
+)
 
 object Miner extends App {
   val search: SearchQuery = SearchQuery().showTags("all").showFields("all").showElements("all")
 
   val contentApiClient: ContentApiClient = GuardianContentClient(sys.env("CONTENT_API_KEY"))
 
+  val format: Format = Format("foo", "bar", "baz")
+
   val allContentSearch = contentApiClient.getResponse(search) map { response =>
     val content = response.results.head
     println(s"${content.webUrl} : ${content.design}")
+  }
+
+  def formatFor(content: Content): Format = {
+
   }
 }

2e74bbf13407ea2f8a484e6806eda4d85f84b945
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-05-10 12:34:31 +0100

    Aggregations on results

--- build.sbt
+++ build.sbt
@@ -4,7 +4,8 @@ ThisBuild / scalaVersion := "2.13.8"
 
 libraryDependencies ++= Seq(
   "com.gu" %% "content-api-client-default" % "18.0.0",
-  "com.gu" %% "content-api-client" % "18.0.0"
+  "com.gu" %% "content-api-client" % "18.0.0",
+  "com.madgag" %% "scala-collection-plus" % "0.11"
 )
 
 lazy val root = (project in file("."))
--- src/main/scala/com/gu/formats/explorer/Miner.scala
+++ src/main/scala/com/gu/formats/explorer/Miner.scala
@@ -3,29 +3,36 @@ package com.gu.formats.explorer
 import com.gu.contentapi.client.model.SearchQuery
 import com.gu.contentapi.client.model.v1.Content
 import com.gu.contentapi.client.utils.CapiModelEnrichment._
+import com.gu.contentapi.client.utils.format.{Design, Display, Theme}
 import com.gu.contentapi.client.{ContentApiClient, GuardianContentClient}
+import com.madgag.scala.collection.decorators._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
+case class ContentFormatData(
+  format: Format,
+  webUrl: String,
+)
+
+
 case class Format(
-  theme: String,
-  design: String,
-  display: String
+  theme: Theme,
+  design: Design,
+  display: Display
 )
 
 object Miner extends App {
-  val search: SearchQuery = SearchQuery().showTags("all").showFields("all").showElements("all")
+  val search: SearchQuery = SearchQuery().showTags("all").showFields("all").showElements("all").pageSize(200)  //todo get blocks?
 
   val contentApiClient: ContentApiClient = GuardianContentClient(sys.env("CONTENT_API_KEY"))
 
-  val format: Format = Format("foo", "bar", "baz")
-
   val allContentSearch = contentApiClient.getResponse(search) map { response =>
-    val content = response.results.head
-    println(s"${content.webUrl} : ${content.design}")
+    val dataForContents = for (content <- response.results) yield ContentFormatData(formatFor(content), content.webUrl)
+    println(dataForContents.groupBy(_.format).mapV(_.size).toSeq.sortBy(_._2).mkString("\n"))
   }
 
-  def formatFor(content: Content): Format = {
 
+  def formatFor(content: Content): Format = {
+    Format(content.theme, content.design, content.display)
   }
 }

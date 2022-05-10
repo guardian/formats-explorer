08bdff47229a51691b333209af64fa17d6d9f31f
Pete Faulconbridge <peter.faulconbridge@gmail.com>
2022-05-10 15:29:42 +0100

    Format results as json

--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ ThisBuild / scalaVersion := "2.13.8"
 libraryDependencies ++= Seq(
   "com.gu" %% "content-api-client-default" % "18.0.0",
   "com.gu" %% "content-api-client" % "18.0.0",
-  "com.madgag" %% "scala-collection-plus" % "0.11"
+  "com.madgag" %% "scala-collection-plus" % "0.11",
+  "com.typesafe.play" %% "play-json" % "2.9.2"
 )
 
 lazy val root = (project in file("."))
--- src/main/scala/com/gu/formats/explorer/Miner.scala
+++ src/main/scala/com/gu/formats/explorer/Miner.scala
@@ -6,6 +6,9 @@ import com.gu.contentapi.client.utils.CapiModelEnrichment._
 import com.gu.contentapi.client.utils.format.{Design, Display, Theme}
 import com.gu.contentapi.client.{ContentApiClient, GuardianContentClient}
 import com.madgag.scala.collection.decorators._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
 
 import java.time.Instant
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,16 +19,28 @@ case class ContentFormatData(
   webUrl: String,
 )
 
+object ContentFormatData {
+  implicit val ContentFormatDataWrites: OWrites[ContentFormatData] = Json.writes[ContentFormatData]
+}
+
+
 case class Format(
   theme: Theme,
   design: Design,
   display: Display
 )
 
+object Format {
+  implicit val ThemeWrites: Writes[Theme] = (o: Theme) => JsString(o.toString)
+  implicit val DesignWrites: Writes[Design] = (o: Design) => JsString(o.toString)
+  implicit val DisplayWrites: Writes[Display] = (o: Display) => JsString(o.toString)
+  implicit val FormatDataWrites: OWrites[Format] = Json.writes[Format]
+}
+
 object Miner extends App {
   val search: SearchQuery =
     SearchQuery()
-      .fromDate(Instant.parse("2022-05-07T00:00:00Z"))
+      .fromDate(Instant.parse("2022-05-09T00:00:00Z"))
       .showTags("all")
       .showFields("all")
       .showElements("all")
@@ -49,6 +64,8 @@ object Miner extends App {
     println(sorted.mkString("\n"))
     println(allContent.size)
     println(sorted.size)
+    val allContentJson = Json.toJson(allContent)
+    println(Json.prettyPrint(allContentJson))
   }
 
   val sortedCounts = allContentSearch.map { _.groupBy(_.format).mapV(_.size).toSeq.sortBy(_._2) }

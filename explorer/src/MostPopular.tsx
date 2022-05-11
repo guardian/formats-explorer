4df49fd9e1584c3d25c2fac1f9729996728c9ec1
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-05-11 13:47:37 +0100

    show all articles

--- explorer/src/MostPopular.tsx
+++ explorer/src/MostPopular.tsx
@@ -1,8 +1,20 @@
 import { groupBy } from 'lodash'
+import { useState } from 'react'
 import { ArticleData } from './App'
 import { ThumbnailCard } from './ThumbnailCard'
 
 export const MostPopular = ({ data }: { data: ArticleData[] }) => {
+    const [visible, setVisible] = useState<string[]>([])
+
+    const onVisibleClick = (name: string) => () => {
+      const index = visible.indexOf(name)
+      if (index !== -1 ) {
+        setVisible(visible.splice(index + 1, 1))
+      } else {
+        setVisible([name, ...visible])
+      }
+    }
+
     const grouped = groupBy(data, (a: ArticleData) => `${a.format.design} - ${a.format.display} - ${a.format.theme}`)
   
     return <div className='container mt-5'>
@@ -19,12 +31,21 @@ export const MostPopular = ({ data }: { data: ArticleData[] }) => {
             return (
               <div>
                 <h4 id={name.replaceAll(' - ', '')}>{name} <span className="badge bg-secondary">{data.length}</span></h4>
-                <div className="row mt-3 mb-5">
+                <div className="row mt-3 mb-3">
                   {
                     list.map((article: ArticleData) => <div className='col-3'><ThumbnailCard article={article}></ThumbnailCard></div>)
                   }
                 </div>
+                {
+                  visible.includes(name) ? 
+                    <div className='row mb-3'>
+                      {data.map(a => <div className='col-6'><li><a href={a.webUrl}>{a.webUrl}</a></li></div>)}
+                    </div>
+                  : ''
+                }
+                <button onClick={onVisibleClick(name)} className='btn btn-outline-dark mb-3'>{visible.includes(name) ? 'Hide' : 'Show'} all articles</button>
               </div>
+              
             )
           })}
         </div>
